e69d7157651008ec75def6d27ee138f5eefadd6c
Xx-IKERO-90-xX <ikerjrh2003@gmail.com>
2026-09-10 12:23:49 +0200

    Cambiamos la configuración del archivo de configuracion de vite

--- vite.config.js
+++ vite.config.js
@@ -17,5 +17,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: 'https://aselectric.github.io/'
+  base: 'aselectric.github.io'
 })
